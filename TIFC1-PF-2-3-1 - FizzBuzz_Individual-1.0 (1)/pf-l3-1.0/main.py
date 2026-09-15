660b20eefc9d65a9c91046ecc624c30c0079f56b
Arge <argegm17@gmail.com>
2026-09-15 15:41:56 -0600

    Add files via upload

---
+++ TIFC1-PF-2-3-1 - FizzBuzz_Individual-1.0 (1)/pf-l3-1.0/main.py
@@ -0,0 +1,11 @@
+
+for numero in range (1,1001):
+    print("El numero es", numero)
+    if numero % divisor == 3
+        print("Fizz")
+    elif numero % divisor == 5
+        print ("Buzz")
+    else numero % divisor == 3,5
+        print ("FizzBuzz")
+        
+
---
+++ TIFC1-PF-3-1-1 - Calculadora_Individual-0.1.0 (1)/pf-l4-main/main.py
@@ -0,0 +1,12 @@
+print ("digite numero uno")
+num1 = int (2)
+print ("digite numero dos")
+num2 = int (5)
+print ("la suma de los dos nuemros es:")
+print (num1 + num2)
+
+num1 = int (10)
+num2 = int (7)
+print ("la suma de los dos nuemros es:")
+print (num1 + num2)
+
